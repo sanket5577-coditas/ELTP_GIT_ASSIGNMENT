5a1009cf5c97dd287487aecca100717505560afc
sanket <sanket.chaudhary@coditas.com>
2024-02-18 21:03:29 +0530

    Day-13 of js

---
+++ JAVASCRIPT/day-13.js
@@ -0,0 +1,17 @@
+// const sumOfTwoNumber =()=>{
+//     const numberOne =22;
+//     const numberTwo =13;
+//     return numberOne+numberTwo
+// }
+
+// console.log(sumOfTwoNumber())
+
+const Multiply = (numberOne) => {
+  const numberTwo = 6;
+  const display = () => {
+    return numberTwo * numberOne;
+  };
+  return display()
+};
+
+console.log(Multiply(4))
