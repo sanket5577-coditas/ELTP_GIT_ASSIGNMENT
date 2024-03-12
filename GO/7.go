b045cc2233939cfc4ba5344c7034b836847e7b6a
sanket <sanket.chaudhary@coditas.com>
2024-03-12 16:10:00 +0530

    shallow clone

---
+++ GO/7.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// IS THIS DEEP CLONE??????
+	matrix := [][]int{
+		{1},
+		{1},
+		{1},
+	}
+
+	fmt.Println(matrix)
+
+	matrix_clone := make([][]int, len(matrix))
+
+	copy(matrix_clone, matrix)
+
+	fmt.Println(matrix_clone)
+
+	matrix_clone[0][0] = 2
+
+	fmt.Println(matrix_clone, matrix)
+
+	matrix_clone = append(matrix_clone, []int{1, 2, 3})
+	fmt.Println(matrix_clone, matrix)
+}
