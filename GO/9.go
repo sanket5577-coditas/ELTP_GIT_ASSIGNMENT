1980372b0fdffedbb3bfa948f81ddab492760600
sanket <sanket.chaudhary@coditas.com>
2024-03-15 16:40:26 +0530

    day-9

--- GO/9.go
+++ GO/9.go
@@ -1,5 +1,52 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+func greet() {
+	fmt.Println("Hello")
+}
+func Nine() {
+	greet()
+
+	ref := greet
+	reference := ref
+
+	reference()
+
+	// fmt.Println(ref)
+
+	add := func() {
+		fmt.Println("i'm addd")
+
+	}
+
+	add()
+	fmt.Println("addition is:", addition(2, 3))
+
+	gotTheName:=GiveMeName("Sanket")
+
+	fmt.Println("Hello",gotTheName)
+
+
+	fmt.Println(RandomNumber())
+
+}
+
+func addition(a, b int) int {
+	return a + b
+}
+
+func GiveMeName (name string) string{
+	return strings.ToUpper(name)
+}
+
+
+func RandomNumber()(number,number2 float64){
+	number = rand.Float64()
+	number2 = rand.Float64()
+	return 
 }
---
+++ GO/test.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// car_loan := EMi(8)
+	// fmt.Println(car_loan)
+
+	fmt.Println(validationPipeline("sanket"))
+	
+}
+
+func validationPipeline(input string) bool{
+	for _, char := range input {
+		if (char < 'a' || char > 'z') && (char < 'A' || char > 'Z') {
+			return false
+		}
+	}
+	return true
+	
+}
+
+// func EMi(r int) int{
+
+// 	something := func (p,n int)  int {
+// 		return p*r*n/100
+// 	}
+// 	return something
+// }
